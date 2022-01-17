b25c021f6bdfd3b2f3262164e5b0dd8b200e9b43
Chloé R <clositi@live.fr>
2022-01-17 20:16:56 +0100

    Footer component created

---
+++ src/components/Footer.js
@@ -0,0 +1,17 @@
+import React from "react";
+import "../style/footer.css"
+
+/**
+ * Footer Component
+ * @returns {JSX} React component
+ */
+
+ function Footer() {
+	return (
+    <footer className="footer">
+      <p className="footer-text">Copyright 2020 Argent Bank</p>
+    </footer>
+	);
+}
+
+export default Footer;
