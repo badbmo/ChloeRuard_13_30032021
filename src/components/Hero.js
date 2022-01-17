cbbf88bd0328326973652c9336839fc1836f4eea
Chloé R <clositi@live.fr>
2022-01-17 21:47:40 +0100

    Hero component created

---
+++ src/components/Hero.js
@@ -0,0 +1,23 @@
+import React from "react";
+import "../style/hero.css";
+
+/**
+ * Footer Component
+ * @returns {JSX} React component
+ */
+
+function Hero() {
+	return (
+		<div className="hero">
+		<section className="hero-content">
+			<h2 className="sr-only">Promoted Content</h2>
+			<p className="subtitle">No fees.</p>
+			<p className="subtitle">No minimum deposit.</p>
+			<p className="subtitle">High interest rates.</p>
+			<p className="text">Open a savings account with Argent Bank today!</p>
+		</section>
+	</div>
+	);
+}
+
+export default Hero;
