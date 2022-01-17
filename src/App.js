24ab5b523fdde091566eecee3318aea74c35b059
Chloé R <clositi@live.fr>
2022-01-17 21:32:58 +0100

    added user route in App

--- src/App.js
+++ src/App.js
@@ -3,13 +3,15 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import User from "./pages/User";
 import Error from "./pages/Error";
 import Home from "./pages/Home";
+import Login from "./pages/Login";
 
 function App() {
 	return (
 		<Router>
 			<Routes>
 				<Route path="/" element={<Home />} />
-				<Route path="user/:userId" element={<User />} />
+				<Route path="/sign-in" element={<Login />} />
+				<Route path="/user" element={<User />} />
 				<Route path="/*" element={<Error />} />
 			</Routes>
 		</Router>
