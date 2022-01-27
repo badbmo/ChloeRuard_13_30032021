69c57aea3a7d61db1761ac6e8ae2501142778df6
Chloé R <clositi@live.fr>
2022-01-27 19:50:31 +0100

    created Reducers

---
+++ src/redux/reducers/isLogged.js
@@ -0,0 +1,14 @@
+const initialState = false;
+
+const isLoggedReducer = (state = initialState, action) => {
+	switch (action.type) {
+		case "LOGGED_IN":
+			return true;
+		case "LOGGED_OUT":
+			return false;
+		default:
+			return state;
+	}
+};
+
+export default isLoggedReducer;
