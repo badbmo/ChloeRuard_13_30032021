278476d3f9a44993a09e4afcda1d67632cefa34a
Chloé R <clositi@live.fr>
2022-01-27 17:26:33 +0100

    FormLogin to display errorMessage and handleSubmit

--- src/components/FormLogin.js
+++ src/components/FormLogin.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import "../style/formLogin.css";
 
 /**
@@ -7,18 +7,33 @@ import "../style/formLogin.css";
  */
 
 function FormLogin() {
+	
+	const [username, setUsername] = useState("");
+	const [password, setPassword] = useState("");
+	const [errorMessage, setErrorMessage] = useState("");
+
+	const handleSubmit = (e) => {
+		e.preventDefault();
+		setErrorMessage("")
+		if (username ==="" || password ===""){
+			setErrorMessage("Please fill the form to connect");
+		}
+		console.log(username, password);
+	};
+
 	return (
 		<section className="sign-in-content">
 			<i className="fa fa-user-circle sign-in-icon"></i>
 			<h1>Sign In</h1>
-			<form>
+			<div className="form__errorMessage">{errorMessage}</div>
+			<form onSubmit={handleSubmit}>
 				<div className="input-wrapper">
 					<label htmlFor="username">Username</label>
-					<input type="text" id="username" />
+					<input type="text" id="username" value={username} onChange={(e) => setUsername(e.target.value)} />
 				</div>
 				<div className="input-wrapper">
 					<label htmlFor="password">Password</label>
-					<input type="password" id="password" />
+					<input type="password" id="password" value={password} onChange={(e) => setPassword(e.target.value)}/>
 				</div>
 				<div className="input-remember">
 					<input type="checkbox" id="remember-me" />
