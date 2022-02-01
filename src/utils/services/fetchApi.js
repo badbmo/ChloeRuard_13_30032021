3cfd803d9334341306d095394b37c0137fb9b73f
Chloé R <clositi@live.fr>
2022-02-01 17:36:49 +0100

    added fetch put to update user infos

--- src/utils/services/fetchApi.js
+++ src/utils/services/fetchApi.js
@@ -48,3 +48,29 @@ export const getUser = (token) => {
 			console.error(error);
 		});
 };
+
+/**
+ * Service to update User Profile to MongoDB with API
+ * @param {string} token auth otken used to put new user's infos
+ * @param {object} credentials new firstName and lastName to upadate user profile
+ * @returns {Promise} data containing new user's infos
+ */
+
+ export const putUser = (token, credentials) => {
+	return fetch("http://localhost:3001/api/v1/user/profile", {
+		method: "PUT",
+		headers: {
+			"Content-Type": "application/json",
+			Authorization: `Bearer ${token}`,
+		},
+		body: JSON.stringify(credentials),
+	})
+		.then((response) => response.json())
+		.then((data) => {
+			console.log(data);
+			return data;
+		})
+		.catch((error) => {
+			console.error(error);
+		});
+};
